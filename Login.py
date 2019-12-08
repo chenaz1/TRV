02b37bea8ae86a28cdcf18c8e93461c984c49a06
YaronPlo <Yaroon15@gmail.com>
2019-12-08 16:24:28 +0200

    Login Func

--- Login.py
+++ Login.py
@@ -3,6 +3,22 @@ from User import *
 from tinydb import TinyDB, Query
 
 
+def NoUser():
+    global screen3
+    screen3 = Toplevel(screen2)
+    screen3.title("")
+    screen.geometry("150x100")
+    Button(screen3, text="Try Again", command = delete).pack()
+    login()
+
+
+
+def delete():
+    screen3.destroy()
+
+def delete1(): #Delete Login Second Screen
+    screen2.destroy()
+
 def reg_user():
     """
     func' that gets users id and name and adds to the database
@@ -15,48 +31,17 @@ def reg_user():
     ID_entry.delete(0, END)
     Label(screen1, text="Register Sucsessfuly added").pack()
 
-
-
-def register():
-    """
-    Register screen
-    :return:
-    """
-    global UserName
-    global PersonID
-    global ID_entry
-    global Username_entry
-    global screen1
-    screen1 = Toplevel(screen)
-    screen1.title("Register")
-    screen1.geometry("300x250")
-    UserName = StringVar()
-    PersonID = StringVar()
-
-    Label(screen1, text="Enter Your Details below: ").pack()
-    Label(screen1, text="").pack()
-    Label(screen1, text="User Name *").pack()
-    Username_entry = Entry(screen1, textvariable=UserName)
-    Username_entry.pack()
-    Label(screen1, text="ID *").pack()
-    ID_entry = Entry(screen1, textvariable=PersonID)
-    ID_entry.pack()
-    Label(screen1, text="").pack()
-    Button(screen1, text="register", width=10, height=1, command=reg_user).pack()
-
 def login_ver():
-    User1=UserNameVerify.get()
+    User1 = UserNameVerify.get()
     ID1 = IDVerify.get()
 
     UserVerify_entry.delete(0,END)
     IDVerify_entry.delete(0, END)
-    if (checkUser(User1, ID1)):
-        {}#ContinueFunc
+    if checkUser(User1, ID1):
+        """""""#login_Sucess()"""
     else:
-        print("Wrong Values")
-        #continueFunc
-
-
+        NoUser()
+        delete1()
 
 
 def login():
@@ -87,6 +72,32 @@ def login():
     Label(screen2, text="").pack()
     Button(screen2, text="Log in", width=10, height=1, command = login_ver).pack()
 
+def register():
+    """
+    Register screen
+    :return:
+    """
+    global UserName
+    global PersonID
+    global ID_entry
+    global Username_entry
+    global screen1
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+    UserName = StringVar()
+    PersonID = StringVar()
+
+    Label(screen1, text="Enter Your Details below: ").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="User Name *").pack()
+    Username_entry = Entry(screen1, textvariable=UserName)
+    Username_entry.pack()
+    Label(screen1, text="ID *").pack()
+    ID_entry = Entry(screen1, textvariable=PersonID)
+    ID_entry.pack()
+    Label(screen1, text="").pack()
+    Button(screen1, text="register", width=10, height=1, command=reg_user).pack()
 
 def main_screen():
     """

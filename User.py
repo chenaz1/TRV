94bc19c7bb1b7a8f662aa6c7c12bea92009ffa44
itay9 <itaydali@gmail.com>
2019-12-08 14:02:20 +0200

    User DataBase

--- User.py
+++ User.py
@@ -3,27 +3,42 @@ from tinydb import TinyDB, Query
 userDB: TinyDB = TinyDB('UserDB.json')
 
 
-def CheckUser(user, id_num):
+def checkUser(name, id_num):
+    # return true if user exist in DB
     tmp = Query()
-    userDB.search((tmp.name == user) & (tmp.id == id_num))
-    if userDB.search(tmp.name.exists()):
+    if userDB.search((tmp.name == name) & (tmp.id == id_num)):
         return True
     return False
 
 
-def RegUser(user, id_num):
-    user = user.lower()
-    user = user.lstrip()
-
-    if id_num[0] == '!':
-        role = 1  # Manager
-    elif id_num[0] == '*':
-        role = 2  # Parent
+def RegUser(name, id_num):
+    # check if user exist, else register
+    name = name.lower()
+    name = name.lstrip()
+    if checkUser(name, id_num):
+        print('user exist in DataBase!')
     else:
-        role = 3  # kid
-    userDB.insert({'name': user, 'id': id_num, 'role': role})
+        if id_num[0] == '!':
+            role = 1  # Manager
+        elif id_num[0] == '*':
+            role = 2  # Parent
+        else:
+            role = 3  # kid
+        userDB.insert({'name': name, 'id': id_num, 'role': role})
 
 
-RegUser('kid', '123456789')
-RegUser('adult', '*123456789')
-RegUser('man', '!123456789')
+def checkPermit(idn, role):
+    tmp = Query()
+    if userDB.search((tmp['role'] == role) & (tmp.id == idn)):
+        return True
+    return False
+
+
+userDB.purge()
+RegUser('man', '!123456')
+RegUser('man','!123456')
+RegUser('man','!1234')
+RegUser('parent', '*123')
+RegUser('kid', '123')
+print(userDB.all())
+print(checkPermit(123456789, 3))

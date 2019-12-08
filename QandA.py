8d634d9e4c47b229f0a49842be20c1cb80d569de
ChenAzulai <chenaz1@ac.sce.ac.il>
2019-12-08 16:44:17 +0200

    questions and answer repository

---
+++ QandA.py
@@ -0,0 +1,21 @@
+from tinydb import TinyDB, Query
+import random
+questions=TinyDB('QandA.json')
+find=Query()
+#questions.purge()
+def new_question(): #enter new question and 3 answer to DB
+    question=input("Enter the question: ")
+    ans1=input("Enter first answer: ")
+    ans2=input("Enter second answer: ")
+    ans3=input("Enter third answer: ")
+    questions.insert({
+        'questions':question,
+        'answer':[ans1,ans2,ans3]
+    })
+    print("The question stock: ", questions.all())
+
+def find_answer(question,answer):
+    if questions.search(find.answer == answer & find.question==question):
+        return True
+    return False
+

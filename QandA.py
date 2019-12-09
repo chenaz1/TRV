bdd78ecd0159c1873017e3479c7de7704b186b8e
ChenAzulai <chenaz1@ac.sce.ac.il>
2019-12-09 16:07:37 +0200

    questions and answer repository 1.2V

--- QandA.py
+++ QandA.py
@@ -1,21 +1,32 @@
-from tinydb import TinyDB, Query
-import random
-questions=TinyDB('QandA.json')
+from tinydb import TinyDB,Query,where
+db=TinyDB('QandA.json')
 find=Query()
-#questions.purge()
+db.purge()
+def iter():
+    it=0
+    def iterator():
+        nonlocal it
+        it=it+1
+        return it
+    return iterator
+
+
 def new_question(): #enter new question and 3 answer to DB
     question=input("Enter the question: ")
     ans1=input("Enter first answer: ")
     ans2=input("Enter second answer: ")
     ans3=input("Enter third answer: ")
-    questions.insert({
-        'questions':question,
-        'answer':[ans1,ans2,ans3]
-    })
-    print("The question stock: ", questions.all())
+    db.insert({'number':iter(),'questions':question,'answer1':ans1,'answer2':ans2,'answer3':ans3})
 
-def find_answer(question,answer):
-    if questions.search(find.question==question & find.answer.all == answer ):
+new_question()
+
+def find_answer():
+    #question=input("Enter the question to find: ")
+    answer=input("Enter the answer to find: ")
+    #result=db.search(where('answer1') == answer)
+    if (db.search(find['answer1'] == answer)) or (db.search(find['answer2'] == answer)) or (db.search(find['answer3'] == answer)):
         return True
     return False
+result=find_answer()
+print(result)
 

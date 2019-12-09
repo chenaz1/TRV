80ebfda5c343fdd542dc24ef6b0f526342288605
ChenAzulai <chenaz1@ac.sce.ac.il>
2019-12-09 17:19:52 +0200

    questions and answer repository and searching in the db

--- QandA.py
+++ QandA.py
@@ -9,22 +9,21 @@ def iter():
         it=it+1
         return it
     return iterator
-
-
+it=iter()
 def new_question(): #enter new question and 3 answer to DB
     question=input("Enter the question: ")
     ans1=input("Enter first answer: ")
     ans2=input("Enter second answer: ")
     ans3=input("Enter third answer: ")
-    db.insert({'number':iter(),'questions':question,'answer1':ans1,'answer2':ans2,'answer3':ans3})
+    db.insert({'number':it(),'questions':question,'answer1':ans1,'answer2':ans2,'answer3':ans3})
 
 new_question()
 
 def find_answer():
-    #question=input("Enter the question to find: ")
+    question=input("Enter the question to find: ")
     answer=input("Enter the answer to find: ")
     #result=db.search(where('answer1') == answer)
-    if (db.search(find['answer1'] == answer)) or (db.search(find['answer2'] == answer)) or (db.search(find['answer3'] == answer)):
+    if ((db.search(find['questions'] == question)) and ((db.search(find['answer1'] == answer)) or (db.search(find['answer2'] == answer)) or (db.search(find['answer3'] == answer)))):
         return True
     return False
 result=find_answer()

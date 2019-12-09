048df847bd718a5d836b48080be1a16e92bf3eb7
yaniv098 <58630354+yaniv098@users.noreply.github.com>
2019-12-09 16:00:42 +0200

    unittest for spellcheck

--- SpellCheck.py
+++ SpellCheck.py
@@ -1,23 +1,18 @@
+import unittest
 
-from tinydb import TinyDB, Query
+import self as self
 from spellchecker import SpellChecker
-import re
-from collections import Counter
-#db = TinyDB('db.json')
-#db.insert({'int' 1, 'char' 'a'})
-#db.insert({'int' 1, 'char' 'b'})
 dir(SpellChecker)
 spell = SpellChecker()
-myPet="dogg"#example for a word with wrong spelling
-correctWord=spell.correction(myPet) #dogg-dog
 
-def checkWord(word,wordAfterCorrect)
+
+def checkWord(word,wordAfterCorrect):
     """
-    param word word from the user
-    param wordAfterCorrect word from the user after spell check
-    return true if the words match else false
+    param word: word from the user
+    param wordAfterCorrect: word from the user after spell check
+    return: true if the words match else false
    """
-    if len(word)<len(wordAfterCorrect):
+    if len(word)<=len(wordAfterCorrect):
         for i in range(len(word)):
             if word[i]!=wordAfterCorrect[i]:
                 return False
@@ -27,6 +22,16 @@ def checkWord(word,wordAfterCorrect)
                  return False
     return True
 
-#answerExist=checkAnswer(correctWord)    #checkAnswer checking if correctWord is exist in the database
 
 
+class testSpellCheck(unittest.TestCase):
+    def test_checkWord(self):
+        word="example"
+        word2=spell.correction(word) #example
+        self.assertTrue(checkWord(word,word2)) #true
+
+        wrong="exemple"
+        fixed=spell.correction(wrong)#example
+        self.assertFalse(checkWord(wrong,fixed))#false
+
+unittest.main()

a6887659f5a9b1d9b80969507fd19d6bcdb3dda8
yaniv098 <58630354+yaniv098@users.noreply.github.com>
2020-01-14 19:01:52 +0200

    created full gui

---
+++ FULL_GUI.py
@@ -0,0 +1,8 @@
+from tinydb import TinyDB, Query
+from spellchecker import SpellChecker
+from random import random
+from tkinter import *
+import time
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
